1e245b98446ef4e7f7b924a8ef1d820709c6c47a
jayaprakash7992 <jayaprakash7992@gmail.com>
2018-07-25 22:50:42 +0530

    Selenium automation in github

---
+++ src/test/java/com/suite1/ChromeTest.java
@@ -0,0 +1,46 @@
+package com.suite1;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+public class ChromeTest {
+	public WebDriver driver;
+	String title="";
+  	String actualTitle="";
+	 
+	  @Test
+	  public void testWebelements() {
+			driver.findElement(By.id("email")).sendKeys("user123");
+			driver.findElement(By.id("password")).sendKeys("password");
+			driver.findElement(By.id("submit-button")).click();
+	  }
+	  
+	  @BeforeMethod
+	  public void launchBrowser() {
+			String exePath="F:\\2018\\Chrome\\chromedriver.exe";
+			String URL="https://stackoverflow.com/users/login";
+			String chromeDriver="webdriver.chrome.driver";
+			System.setProperty(chromeDriver, exePath);
+			//System.out.println("opening the browser");
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+	 
+	      //Put a Implicit wait, this means that any search for elements on the page could take the time the implicit wait is set for before throwing exception
+	 
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			driver.get(URL);
+	  }
+	 
+	  @AfterMethod
+	  public void terminateBrowser() {
+	      driver.quit();
+	 
+	  }
+}
